e7a6f866e320975f102ab32780b6c8934e679ff2
hucodelab <hvallejo.77@gmail.com>
2022-02-26 01:52:38 -0300

    feb 26 22

--- app/run.py
+++ app/run.py
@@ -122,9 +122,9 @@ def go():
     )
 
 
-def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
+#def main():
+#    app.run(host='0.0.0.0', port=3001, debug=True)
 
 
-if __name__ == '__main__':
-    main()
+#if __name__ == '__main__':
+#    main()
